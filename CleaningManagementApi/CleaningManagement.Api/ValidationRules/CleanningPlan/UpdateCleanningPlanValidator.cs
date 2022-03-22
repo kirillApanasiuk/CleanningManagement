aedcb5a15a9813505e89da0d4c9d3096f5156128
Kirill Ovsyannikov <kirill.ovsyannikov@cogniteq.com>
2022-03-22 19:57:48 +0300

    feat: added tests to update cleanning plan

--- CleaningManagementApi/CleaningManagement.Api/ValidationRules/CleanningPlan/UpdateCleanningPlanValidator.cs
+++ CleaningManagementApi/CleaningManagement.Api/ValidationRules/CleanningPlan/UpdateCleanningPlanValidator.cs
@@ -1,30 +1,29 @@
 ﻿using CleaningManagement.Application.UseCases.CleanningPlan.DTOs;
 using CleanningManagement.Resources;
 using FluentValidation;
-using System;
 
 namespace CleaningManagement.Api.ValidationRules.CleanningPlan
 {
-    public class UpdateCleanningPlanValidator : AbstractValidator<UpdateCleanningPlanDto>
+    public class UpdateCleanningPlanValidator : AbstractValidator<UpdateCleanningPlanRequestDto>
     {
         public UpdateCleanningPlanValidator()
         {
             RuleFor(createCPDto => createCPDto.Title)
                    .Cascade(CascadeMode.StopOnFirstFailure)
                    .NotEmpty()
-                   .WithMessage(string.Format(ValidationErrors.FieldRequired, nameof(UpdateCleanningPlanDto.Title)))
+                   .WithMessage(string.Format(ValidationErrors.FieldRequired, nameof(UpdateCleanningPlanRequestDto.Title)))
                    .MaximumLength(256)
-                   .WithMessage(string.Format(ValidationErrors.MaxLength, nameof(UpdateCleanningPlanDto.Title), 256));
+                   .WithMessage(string.Format(ValidationErrors.MaxLength, nameof(UpdateCleanningPlanRequestDto.Title), 256));
 
             RuleFor(createCPDto => createCPDto.CustomerId)
                 .NotEmpty()
-               .WithMessage(string.Format(ValidationErrors.NotDefaultTypeValue, nameof(UpdateCleanningPlanDto.CustomerId), int.MinValue));
+               .WithMessage(string.Format(ValidationErrors.NotDefaultTypeValue, nameof(UpdateCleanningPlanRequestDto.CustomerId), 0));
 
             When(createCPDto => !string.IsNullOrEmpty(createCPDto.Description), () =>
             {
                 RuleFor(createCPDto => createCPDto.Description)
                    .MaximumLength(512)
-                   .WithMessage(string.Format(ValidationErrors.MaxLength, nameof(UpdateCleanningPlanDto.Description), 512));
+                   .WithMessage(string.Format(ValidationErrors.MaxLength, nameof(UpdateCleanningPlanRequestDto.Description), 512));
             });
         }
     }
--- CleaningManagementApi/CleanningManagement.Api.UnitTests/ValidationRules/CleanningPlan/UpdateCleaningPlanValidatorTests.cs
+++ CleaningManagementApi/CleanningManagement.Api.UnitTests/ValidationRules/CleanningPlan/UpdateCleaningPlanValidatorTests.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using CleaningManagement.Api.ValidationRules.CleanningPlan;
+using CleaningManagement.Application.UseCases.CleanningPlan.DTOs;
+using CleanningManagement.Resources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace CleanningManagement.Api.UnitTests.ValidationRules.CleanningPlan
 {
-    internal class UpdateCleaningPlanValidatorTests
+    public class UpdateCleaningPlanValidatorTests
     {
+        private readonly UpdateCleanningPlanValidator _validator = new UpdateCleanningPlanValidator();
+
+        [Fact]
+        public void TitleNotEmpty_WithEmptyTitle_ValidationFails()
+        {
+            //arrange 
+            var cleanningPlanDtoWithEmptyTitle = new UpdateCleanningPlanRequestDto { Title = string.Empty };
+            //act
+            var validationResult = _validator.Validate(cleanningPlanDtoWithEmptyTitle);
+            //assert
+            Assert.Contains(validationResult.Errors
+                .Select(x => x.ErrorMessage),
+                x => x == string.Format(ValidationErrors.FieldRequired, nameof(UpdateCleanningPlanRequestDto.Title)));
+        }
+
+        [Fact]
+        public void Title_GreaterMaxLength_ValidationFails()
+        {
+            //arrange
+            var stringGreaterMaxLength = new String('*', 257);
+            var cleanningPlanUpdateDtoWithGreaterTitleStringLength = new UpdateCleanningPlanRequestDto { Title = stringGreaterMaxLength };
+            //act
+            var validationResult = _validator.Validate(cleanningPlanUpdateDtoWithGreaterTitleStringLength);
+            //assert
+            Assert.Contains(validationResult.Errors.Select(x => x.ErrorMessage), x => x == string.Format(ValidationErrors.MaxLength, nameof(UpdateCleanningPlanRequestDto.Title), 256));
+        }
+
+        [Fact]
+        public void CustomerId_IsEmptyOrDefault_ValidationFails()
+        {
+            //arrange 
+            var cleanningPlanUpdateDtoWithEmptyCustomerId = new UpdateCleanningPlanRequestDto { CustomerId = 0, Title = "Test title" };
+            //act
+            var validationResult = _validator.Validate(cleanningPlanUpdateDtoWithEmptyCustomerId);
+            //assert
+            Assert.Contains(validationResult.Errors
+                .Select(x => x.ErrorMessage),
+                x => x == string.Format(ValidationErrors.NotDefaultTypeValue, nameof(UpdateCleanningPlanRequestDto.CustomerId), 0));
+        }
+
+        [Fact]
+        public void Description_IsOptional_ValidationPass()
+        {
+            //arrange 
+            var cleanningPlanDtoWithEmptyCustomerId = new UpdateCleanningPlanRequestDto { Title = "Test title", CustomerId = 1 };
+            //act
+            var validationResult = _validator.Validate(cleanningPlanDtoWithEmptyCustomerId);
+            //assert
+            Assert.True(!validationResult.Errors.Any());
+        }
+
+
+
+
+
     }
 }
